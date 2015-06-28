129a734a6a436ffd0e63141e9345c8d253c8dab1
Pranit Bauva <pranit.bauva@gmail.com>
2015-06-28 11:45:42 +0530

    Use a function for the conversion

--- K_R/celsius_fahranheit.c
+++ K_R/celsius_fahranheit.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
 #define LOWER 0.0
 #define UPPER 100.0
 #define STEP 20.0
+
+double to_fahranheit(double celsius);
 
 int main(int argc, int *argv[]){
     printf("Celsius\t Fahranheit\n");
     float i;
     for(i = LOWER; i <= UPPER; i = i + STEP){
-        printf("%6.1f \t %6.1f\n", i, ((i+32.0)*9.0/5.0));
+        printf("%6.1f \t %6.1f\n", i, to_fahranheit(i));
     }
     return 0;
+}
+
+double to_fahranheit(double celsius){
+    return ((celsius * 9 / 5) + 32);
 }
